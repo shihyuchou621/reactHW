123b78dc11cd9bd39470f5361991c9781869cdf3
shihyuchou <kellyu621@gmail.com>
2020-06-11 23:39:33 +0800

    hw12

---
+++ src/HW12/Tags.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class Tags extends Component {
+  render() {
+    const {
+      tag,
+      content,
+      onClick,
+      isChoice,
+    } = this.props;
+
+    return (
+        <div className={isChoice?"tags tagChoice":"tags"} onClick={onClick} data-content={content}>{tag}</div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import HW from './HW11/index'; 
+import HW from './HW12/index'; 
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
