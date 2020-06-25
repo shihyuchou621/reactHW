49d7d5ac7a359229cd441c0c33226119dd6557f6
shihyuchou <kellyu621@gmail.com>
2020-06-25 21:56:51 +0800

    HW17

---
+++ src/HW17/Master.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+const getRandom = (max, min = 0) => {
+  return ~~(Math.random() * (max - min + 1)) + min;
+}
+
+export default class Master extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      num: getRandom(9)
+    }
+  }
+
+  tick = () => {
+    this.setState({ num: getRandom(9)})
+    setTimeout(() => {
+      this.tick();
+    }, getRandom(1000, 500));
+  }
+
+  componentDidMount() {
+    this.tick();
+  }
+  
+  render() {
+    return (
+      <div>
+        {this.state.num}
+      </div>
+    )
+  }
+}
