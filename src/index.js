e308bd7e55d5b6ae2882f0ea6d6fa6c0742e2ae3
shihyuchou <kellyu621@gmail.com>
2020-10-30 16:40:02 +0800

    CICD-HW23

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import HW from './hooks/HW25';
+import HW from './hooks/HW23';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
